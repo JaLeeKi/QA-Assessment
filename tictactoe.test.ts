519deb4f4cb1ff675573e6e30e2f94a32b92ff81
JaLeeKi <jadekirkham@gmail.com>
2021-06-30 11:05:18 -0600

    seperated tests into different tests for easier reading

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -17,16 +17,22 @@ test('I can start a game', async () => {
     let button = await (await driver).findElement(By.id('start-game'));
     await button.click();
     await driver.sleep(500);
+});
 
+test('Select top left square', async () => {
     let cell1 = await (await driver).findElement(By.id('cell-0'));
     await cell1.click();
     await driver.sleep(1000);
+})
 
+test('Select middle square', async () => {
     let cell2 = await (await driver).findElement(By.id('cell-4'));
     await cell2.click();
     await driver.sleep(1000);
+})
 
+test('Select bottom right square', async () => {
     let cell3 = await (await driver).findElement(By.id('cell-8'));
     await cell3.click();
     await driver.sleep(1000);
-});
+})
